e98299d11be9752b8ae0d252c27ade77b168e6d1
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-08-10 14:23:35 +0800

    feat: add storybook to the project
    Storybook is used to showcase the UI design and to form a style guide.
    
    Github issue #4

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = (storybookBaseConfig, configType, defaultConfig) => {
+  defaultConfig.module.rules.push({
+    test: [/\.stories\.js$/, /index\.js$/],
+    loaders: [require.resolve('@storybook/addon-storysource/loader')],
+    include: [path.resolve(__dirname, '../src')],
+    enforce: 'pre',
+  })
+
+  defaultConfig.module.rules.push({
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'eslint-loader',
+    enforce: 'pre',
+    options: {
+      fix: true,
+      formatter: require('eslint-friendly-formatter'),
+    },
+  })
+
+  defaultConfig.resolve = {
+    alias: {
+      '@': path.join(__dirname, '../src/renderer'),
+      vue$: 'vue/dist/vue.esm.js',
+    },
+    extensions: ['.js', '.vue', '.json', '.css', '.node'],
+  }
+
+  return defaultConfig
+}
