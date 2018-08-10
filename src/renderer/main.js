fcf9558af2ac4d7d0859857fb84048390c9001e2
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-08-10 21:59:35 +0800

    feat: add VueMDCAdapter to Vue
    There's also a theme.scss file added that can be used to define custom
    theme.
    
    Github issue #4

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,8 +4,14 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import VueMDCAdapter from 'vue-mdc-adapter'
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
+
+Vue.use(VueMDCAdapter)
+
+import './theme.scss'
 
 /* eslint-disable no-new */
 new Vue({
