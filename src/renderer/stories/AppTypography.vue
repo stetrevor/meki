67ad2a973106028e5407c873cdb9ed3b26538ba3
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-08-12 20:44:07 +0800

    feat: add theme typography stories
    Since storybook background addon is not working. A knob is used to
    change the background of the typography, and the color of the texts will
    change with it.
    
    Github issue #5

---
+++ src/renderer/stories/AppTypography.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="['app-typography', ...colorClasses]">
+    <mdc-text typo="headline1">Headline 1</mdc-text>
+    <mdc-text typo="headline2">Headline 2</mdc-text>
+    <mdc-text typo="headline3">Headline 3</mdc-text>
+    <mdc-text typo="headline4">Headline 4</mdc-text>
+    <mdc-text typo="headline5">Headline 5</mdc-text>
+    <mdc-text typo="headline6">Headline 6</mdc-text>
+    <mdc-text typo="subtitle1">Subtitle 1</mdc-text>
+    <mdc-text typo="subtitle2">Subtitle 2</mdc-text>
+    <mdc-text typo="body1">Body 1</mdc-text>
+    <mdc-text typo="body2">Body 2</mdc-text>
+    <mdc-text typo="caption">Caption</mdc-text>
+    <mdc-text typo="button">Button</mdc-text>
+    <mdc-text typo="overline">Overline</mdc-text>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTypography',
+
+  props: {
+    colors: {
+      type: String,
+      default: 'background background',
+    },
+  },
+
+  computed: {
+    colorClasses() {
+      const [bg, fg] = this.colors.split(' ')
+      return [`mdc-theme--${bg}`, `mdc-theme--on-${fg}`]
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.app-typography {
+  border-radius: 4px;
+  padding: 40px;
+}
+</style>
--- src/renderer/stories/theme.stories.js
+++ src/renderer/stories/theme.stories.js
@@ -1,11 +1,29 @@
 import { storiesOf } from '@storybook/vue'
-import backgrounds from '@storybook/addon-knobs'
+import { withKnobs, select } from '@storybook/addon-knobs'
 import Vue from 'vue'
 
 import AppTheme from './AppTheme'
+import AppTypography from './AppTypography'
 
 Vue.component('AppTheme', AppTheme)
+Vue.component('AppTypography', AppTypography)
 
 const theme = storiesOf('App Theme', module)
 
 theme.add('Colors', () => ({ template: '<app-theme />' }))
+
+theme.addDecorator(withKnobs)
+theme.add('Typography', () => {
+  const colors = select(
+    'Background Color',
+    {
+      'primary-bg primary': 'Primary',
+      'secondary-bg secondary': 'Secondary',
+      'surface surface': 'Surface',
+      'background background': 'Background',
+    },
+    'background background',
+  )
+
+  return { template: `<app-typography colors="${colors}"/>` }
+})
