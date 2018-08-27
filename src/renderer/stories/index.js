1fabbbd3976ef5f9cc4899366e3327a0c4c7ef8e
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-08-27 12:49:54 +0800

    feat: Update story 'Paw' to use centered addon

--- src/renderer/stories/index.js
+++ src/renderer/stories/index.js
@@ -1,5 +1,8 @@
 import { storiesOf } from '@storybook/vue'
+import centered from '@storybook/addon-centered'
 
 const homePage = storiesOf('Paw', module)
+
+homePage.addDecorator(centered)
 
 homePage.add('App Name', () => ({ template: '<h1>Paw: Media Manager</h1>' }))
