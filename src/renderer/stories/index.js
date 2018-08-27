f6f7051e29e1689ba3f09a0a94aa490f5b0a4448
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-08-27 12:58:54 +0800

    feat: Add app logo story
    Showcase how to use svg in Vue template.
    
    Github issue #11

--- src/renderer/stories/index.js
+++ src/renderer/stories/index.js
@@ -6,3 +6,11 @@ const homePage = storiesOf('Paw', module)
 homePage.addDecorator(centered)
 
 homePage.add('App Name', () => ({ template: '<h1>Paw: Media Manager</h1>' }))
+homePage.add('App Logo', () => {
+  const Logo = require('../assets/logo.svg')
+
+  return {
+    template: '<logo width="256" height="256" />',
+    components: { Logo },
+  }
+})
