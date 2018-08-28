b532e8b4bddfed1fa411531b266f1c3b89ff6afe
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-08-28 15:17:55 +0800

    feat: Add app initial screen story
    Github issue #10

--- src/renderer/stories/index.js
+++ src/renderer/stories/index.js
@@ -14,3 +14,15 @@ homePage.add('App Logo', () => {
       '<svg width="256" height="256" class="theme-text-color-secondary"><use xlink:href="#logo"></use></svg>',
   }
 })
+
+homePage.add('Initial Screen', () => {
+  require('../assets/logo.svg')
+
+  return {
+    template: `
+      <div class="theme-bg-color-primary theme-text-color-secondary"
+           style="width: calc(100vw - 20px); height: calc(100vh - 20px); display: flex; align-items: center; justify-content: center">
+        <svg width="192" height="288"><use xlink:href="#logo"></use></svg>
+      </div>`,
+  }
+})
