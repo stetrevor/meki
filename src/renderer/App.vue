bbad0297ad3cfccbbe38fe685fdb9937a708d732
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-08-29 13:57:50 +0800

    feat: Change app name to Paw everywhere
    There should be a search in the project to find all old app name
    instances. Those are the ones left out. Fix them now.
    
    Github issue #8

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'Meki',
+  name: 'Paw',
 }
 </script>
 
--- test/e2e/specs/Launch.spec.js
+++ test/e2e/specs/Launch.spec.js
@@ -6,7 +6,7 @@ describe('Launch', function() {
 
   it('shows the proper application title', function() {
     return this.app.client.getTitle().then(title => {
-      expect(title).to.equal('meki')
+      expect(title).to.equal('Paw')
     })
   })
 })
