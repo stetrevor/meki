bf96e276ec28558a2bb21adc2650e94a510bc7b7
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-08-29 14:53:10 +0800

    feat: Run lint:fixall to format files
    Github issue #12

--- test/e2e/utils.js
+++ test/e2e/utils.js
@@ -2,22 +2,22 @@ import electron from 'electron'
 import { Application } from 'spectron'
 
 export default {
-  afterEach () {
+  afterEach() {
     this.timeout(10000)
 
     if (this.app && this.app.isRunning()) {
       return this.app.stop()
     }
   },
-  beforeEach () {
+  beforeEach() {
     this.timeout(10000)
     this.app = new Application({
       path: electron,
       args: ['dist/electron/main.js'],
       startTimeout: 10000,
-      waitTimeout: 10000
+      waitTimeout: 10000,
     })
 
     return this.app.start()
-  }
+  },
 }
