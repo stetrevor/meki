476c647b09154f672f13b28de4fd0e94f2cfe5e1
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-08-30 14:12:12 +0800

    feat: Add homepage icon story
    Github issue #15

--- src/renderer/stories/homepage.stories.js
+++ src/renderer/stories/homepage.stories.js
@@ -4,3 +4,23 @@ import centered from '@storybook/addon-centered'
 const homepage = storiesOf('Home Page', module)
 
 homepage.add('Add Media Button', () => ({ template: '<add-media-button/>' }))
+
+homepage.add('Icons', () => {
+  require('../assets/icons/icon-settings.svg')
+  require('../assets/icons/icon-search.svg')
+  require('../assets/icons/icon-selection-mode.svg')
+  require('../assets/icons/icon-fullscreen.svg')
+  require('../assets/icons/icon-fullscreen-exit.svg')
+
+  return {
+    template: `
+      <div class="theme-text-color-on-primary theme-bg-color-primary"
+        style="height: calc(100vh - 24px * 2); padding: 24px 0; display: grid; grid-gap: 24px; grid-template-columns: repeat(auto-fill, 48px); grid-auto-rows: 48px; justify-content: center">
+        <svg width="48" height="48"><use xlink:href="#icon-settings"/></svg>
+        <svg width="48" height="48"><use xlink:href="#icon-search"/></svg>
+        <svg width="48" height="48"><use xlink:href="#icon-selection-mode"/></svg>
+        <svg width="48" height="48"><use xlink:href="#icon-fullscreen"/></svg>
+        <svg width="48" height="48"><use xlink:href="#icon-fullscreen-exit"/></svg>
+      </div>`,
+  }
+})
