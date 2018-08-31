4b37c2c43fc324d772d6b2bdcdccf273a442b203
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-08-31 14:25:42 +0800

    feat: Add NavItem stories
    Github issue #15

--- src/renderer/stories/homepage.stories.js
+++ src/renderer/stories/homepage.stories.js
@@ -3,3 +3,13 @@ import { storiesOf } from '@storybook/vue'
 const amb = storiesOf('Home Page | Add Media Button', module)
 
 amb.add('Add Media Button', () => ({ template: '<add-media-button/>' }))
+
+const nav = storiesOf('Home Page | Navigation')
+
+nav.add('NavItem', () => ({
+  template: `
+    <div class="theme-bg-color-primary" style="width: 100vw; height: 100vh; padding: 48px 0; display: flex; flex-direction: column">
+      <nav-item>Recents</nav-item>
+      <nav-item class="nav-item--active">Movies</nav-item>
+    </div>`,
+}))
