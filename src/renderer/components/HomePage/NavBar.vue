377d532f939c3fe5c623123a1fc8f96213c6b2bb
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-08-31 14:55:07 +0800

    feat: Create NavBar
    NavBar uses NavItem to layout content.
    
    Github issue #15

---
+++ src/renderer/components/HomePage/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="nav-bar">
+    <nav-item v-for="item in navItems" 
+              :key="item" 
+              :class="{'nav-item--active': activeNavItem === item}" 
+              @click.native="activeNavItem = item">
+      {{ item }}
+    </nav-item>
+  </div>
+</template>
+
+<script>
+import NavItem from './NavItem'
+
+export default {
+  name: 'NavBar',
+
+  components: {
+    NavItem,
+  },
+
+  props: {
+    navItems: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      activeNavItem: this.navItems[0],
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.nav-bar {
+  display: grid;
+  grid-gap: 24px;
+  grid-template-columns: 1fr;
+}
+</style>
