5880f549bed954401ee74fa5804964fbe1aae53c
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-09-02 17:53:30 +0800

    fix: Remove wrong styles from ProgressBar stories
    Github issue #16

--- src/renderer/stories/base.stories.js
+++ src/renderer/stories/base.stories.js
@@ -77,7 +77,7 @@ base.add('Overlay Icon Button', () => {
 
 base.add('Progress Bar', () => ({
   template: `
-  <div class="theme-bg-color-primary" style="padding: 48px; width: calc(100vw - 48px * 2); height: calc(100vh - 48px * 2); display: grid; grid-gap: 24px; just">
+  <div class="theme-bg-color-primary" style="padding: 48px; width: calc(100vw - 48px * 2); height: calc(100vh - 48px * 2); display: grid; grid-gap: 24px">
     <progress-bar :progress="32" :max="100"/>
     <progress-bar :colored="true" :progress="64" :max="100"/>
     <progress-bar :colored="true" :progress="64" :max="100" class="theme-bg-color-background"/>
