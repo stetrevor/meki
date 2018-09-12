8dd6ea31f04ce5fe0afdc6ae39a6d8fc6c28a194
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-09-12 15:56:01 +0800

    feat: Use 'vue-rx' as Vue plugin
    Github issue #22

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,10 +4,14 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import VueRx from 'vue-rx'
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
 import './theme.scss'
+
+Vue.use(VueRx)
 
 /* eslint-disable no-new */
 new Vue({
