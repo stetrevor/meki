fa6f568ac66baa24f03aa0c7d36f3a3492eef9a2
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-09-17 18:17:00 +0800

    feat: Create FullscreenToggle
    FullscreenToggle toggles window fullscreen mode using electron api. The
    previous fullscreen implementation uses browser api and it doesn't work
    when switch between pages.

---
+++ src/renderer/components/Base/FullscreenToggle.vue
@@ -0,0 +1,28 @@
+<template>
+  <icon-toggle-button :toggled="fullscreen" 
+                      class="fullscreen-toggle"
+                      icon-normal="fullscreen" 
+                      icon-toggled="fullscreen-exit"
+                      @click.native="toggleFullscreen"/>
+</template>
+
+<script>
+import { remote } from 'electron'
+
+import { mapGetters, mapActions } from 'vuex'
+
+import IconToggleButton from './IconToggleButton'
+
+import '../../assets/icons/icon-fullscreen.svg'
+import '../../assets/icons/icon-fullscreen-exit.svg'
+
+export default {
+  name: 'FullscreenToggle',
+
+  components: { IconToggleButton },
+
+  computed: mapGetters(['fullscreen']),
+
+  methods: mapActions(['toggleFullscreen']),
+}
+</script>
