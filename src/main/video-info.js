543064bc1f93c7b96b8ccf0b941dce3feb473ad6
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-09-21 17:47:45 +0800

    feat: Implement getVideoInfo
    getVideoInfo takes the path of a video file and the path of the output
    directory, and returns a promise, when resolved, contains the video
    runtime and path to the generated thumbnail relative to the output
    directory.
    
    Github issue #28

---
+++ src/main/video-info.js
@@ -0,0 +1,44 @@
+import ffmpeg from 'fluent-ffmpeg'
+import ffmpegBin from 'ffmpeg-static'
+import ffprobeBin from 'ffprobe-static'
+import hash from 'hash.js'
+
+ffmpeg.setFfmpegPath(ffmpegBin.path)
+ffmpeg.setFfprobePath(ffprobeBin.path)
+const sha256 = hash.sha256()
+
+const generateThumbnail = (filePath, output) => {
+  return new Promise((resolve, reject) => {
+    const backdropPath = sha256.update(filePath).digest('hex') + '.png'
+
+    ffmpeg(filePath)
+      .on('end', () => resolve(backdropPath))
+      .on('error', err => reject(err))
+      .takeScreenshots({
+        count: 1,
+        timestamps: ['50%'],
+        folder: output,
+        filename: backdropPath,
+      })
+  })
+}
+
+const getVideoRuntime = filePath => {
+  return new Promise((resolve, reject) => {
+    ffmpeg(filePath).ffprobe((err, data) => {
+      if (err) reject(err)
+
+      const runtime = data.format.duration
+      resolve(runtime)
+    })
+  })
+}
+
+const getVideoInfo = (filePath, output) => {
+  return Promise.all([
+    getVideoRuntime(filePath),
+    generateThumbnail(filePath, output),
+  ]).then(([runtime, backdropPath]) => ({ runtime, backdropPath }))
+}
+
+export default getVideoInfo
