93e0515aa5d5465392170edfe6a9397105757d5e
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-09-22 14:53:29 +0800

    test: Add video-info setup tests
    Github issue #28

--- test/unit/specs/video-info.spec.js
+++ test/unit/specs/video-info.spec.js
@@ -5,8 +5,9 @@ import ffmpeg from 'fluent-ffmpeg'
 import ffmpegBin from 'ffmpeg-static'
 import ffprobeBin from 'ffprobe-static'
 import hash from 'hash.js'
+import { Subject } from 'rxjs'
 
-import getVideoInfo from '../../../src/main/video-info'
+import { getVideoInfo, setup } from '../../../src/main/video-info'
 
 describe('ffmpeg-static', () => {
   it('should have the path to ffmpeg', () => {
@@ -74,6 +75,24 @@ describe('hash.js', () => {
   })
 })
 
+describe('rxjs', function() {
+  describe('Subject', function() {
+    it('can have next called outside', function() {
+      const s = new Subject()
+      let value
+
+      s.subscribe(v => (value = v))
+
+      s.next('a')
+      expect(value).to.equal('a')
+      s.next('b')
+      expect(value).to.equal('b')
+      s.next('c')
+      expect(value).to.equal('c')
+    })
+  })
+})
+
 describe('video-info', function() {
   it('should get video info: runtime and thumbnail', function() {
     const videData = { _id: '1234', filePath: path.join(__dirname, 'test.mp4') }
@@ -86,11 +105,45 @@ describe('video-info', function() {
 
       expect(info).to.deep.equal(
         Object.assign({}, videData, {
-        runtime: 10.022,
-        backdropPath: h,
+          runtime: 10.022,
+          backdropPath: h,
         }),
       )
-      })
     })
+  })
+
+  it('should accept input function and output function', function(done) {
+    const paths = [1, 2, 3].map(i =>
+      path.join(__dirname, 'fixture-videos', `test${i}.mp4`),
+    )
+    console.log('paths', paths)
+    const hashs = [
+      '352a61d787a42093e2a5ade9b45a8e0d02b7e494ede001f1a95f2d25e56d0d59.png',
+      '1be0f87b04526ec0451ee62a2bc0f22ccd9f3527d24c6a53b9772b0eb5b31842.png',
+      '8b830981104bd1736c1e156e72a124afa8fedd5ebb88f6da3a3267c03231f6e8.png',
+    ]
+    console.log('hashs', hashs)
+    const fixture = paths.map((filePath, i) => ({ _id: i + 1, filePath }))
+    let expected = fixture.map((d, i) =>
+      Object.assign({}, d, {
+        runtime: 1.022,
+        backdropPath: hashs[i],
+      }),
+    )
+
+    setup(
+      path.join(__dirname, 'temp'),
+      videoData$ => {
+        fixture.forEach(d => videoData$.next(d))
+        videoData$.complete()
+      },
+      info => {
+        const ex = expected.find(r => r._id === info._id)
+        expect(info).to.deep.equal(ex)
+        expected = expected.filter(r => r._id !== info._id)
+      },
+      null,
+      done,
+    )
   })
 })
