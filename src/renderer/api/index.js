252a02b50102b87bb3c732cbab7da6180c3112f7
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-09-23 21:26:28 +0800

    feat: Implement media api
    Media api.addMediaItem returns the added media item, api.updateMedia
    returns an array of media items updated, and api.deleteMedia returns
    an array with two elements, first being the ids array, and the second
    being the numbers of media items deleted.
    
    Github issue #31

--- src/renderer/api/index.js
+++ src/renderer/api/index.js
@@ -1,11 +1,36 @@
 import { ipcRenderer } from 'electron'
 
+import { promisify } from 'util'
+
+import db from '../db'
+
 let setupVideoInfo = false
+
+function promisifyUpdate(dbCollection) {
+  return (...params) => {
+    return new Promise((resolve, reject) => {
+      dbCollection.update(
+        ...params,
+        { returnUpdatedDocs: true, multi: true },
+        (err, _, affectedDocs, __) => {
+          if (err) reject(err)
+
+          resolve(affectedDocs)
+        },
+      )
+    })
+  }
+}
+
+const media = {
+  insert: promisify((...params) => db.media.insert(...params)),
+  update: promisifyUpdate(db.media),
+  deleteMedia: promisify((...params) => db.media.remove(...params)),
+}
 
 export default {
   async addMediaItem(data) {
-    console.warn('api.addMediaItem NOT Implemented')
-    return data
+    return await media.insert(data)
   },
 
   getVideoInfo(videoData, callback) {
@@ -17,17 +42,14 @@ export default {
   },
 
   async updateMedia(ids, updates) {
-    console.warn('api.addMediaItem NOT Implemented')
-    return await updates
+    return await media.update({ _id: { $in: ids } }, { $set: updates })
   },
 
   async deleteMedia(ids) {
-    const deleted = ids.reduce((acc, curr) => {
-      acc.push({ _id: curr })
-      return acc
-    }, [])
-    console.warn('api.deleteMedia NOT Imlemented', deleted)
-
-    return deleted
+    const numRemoved = await media.deleteMedia(
+      { _id: { $in: ids } },
+      { multi: true },
+    )
+    return [ids, numRemoved]
   },
 }
