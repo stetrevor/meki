a4fde3822e0453832b303fdd176e829b97a92c4a
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-09-23 21:30:56 +0800

    test: Add media api tests
    Github issue #31

---
+++ test/unit/specs/api.spec.js
@@ -0,0 +1,41 @@
+import api from '../../../src/renderer/api'
+import db from '../../../src/renderer/db'
+
+describe('api', function() {
+  describe('media', function() {
+    beforeEach(function(done) {
+      db.media.remove({}, { multi: true }, done)
+    })
+
+    it('addMediaItem', function() {
+      return api
+        .addMediaItem({ _id: '1', filePath: 'abc.mp4' })
+        .then(mediaItem =>
+          expect(mediaItem).to.deep.equal({ _id: '1', filePath: 'abc.mp4' }),
+        )
+    })
+
+    it('updateMedia', function() {
+      return api.addMediaItem({ _id: '1', filePath: 'abc.mp4' }).then(() => {
+        return api.updateMedia(['1'], { favorite: true }).then(mediaItem => {
+          expect(mediaItem).to.deep.equal([
+            {
+              _id: '1',
+              favorite: true,
+              filePath: 'abc.mp4',
+            },
+          ])
+        })
+      })
+    })
+
+    it('deleteMedia', function() {
+      return api.addMediaItem({ _id: '1', filePath: 'abc.mp4' }).then(() => {
+        return api.deleteMedia(['1']).then(([ids, numRemoved]) => {
+          expect(ids).to.deep.equal(['1'])
+          expect(numRemoved).to.equal(1)
+        })
+      })
+    })
+  })
+})
