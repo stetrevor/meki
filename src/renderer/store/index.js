013c8ee08e5f435a49496e6035424a628b3c6962
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-09-26 12:38:15 +0800

    feat: Implement PlayerPage state and action
    Implement action switchCurrentPlayingEpisode and state
    currentPlayingEpisode.
    
    Github issue #33

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -5,7 +5,26 @@ import modules from './modules'
 
 Vue.use(Vuex)
 
+const state = {
+  currentPlayingEpisode: null,
+}
+
+const mutations = {
+  SWITCH_CURRENT_PLAYING_EPISODE(state, episode) {
+    state.currentPlayingEpisode = episode
+  },
+}
+
+const actions = {
+  switchCurrentPlayingEpisode({ commit }, episode) {
+    commit('SWITCH_CURRENT_PLAYING_EPISODE', episode)
+  },
+}
+
 export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
   modules,
   strict: process.env.NODE_ENV !== 'production',
 })
