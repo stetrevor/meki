4df5c7135eeb7471fac3d209f81d53912d0fbf58
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-09-27 14:02:07 +0800

    feat: Update settings in PlayerPage store
    Github issue #30

--- src/renderer/store/modules/PlayerPage.js
+++ src/renderer/store/modules/PlayerPage.js
@@ -1,3 +1,7 @@
+import { remote } from 'electron'
+
+const settings = remote.require('electron-settings')
+
 const state = {
   volume: 100,
   muted: false,
@@ -15,10 +19,12 @@ const mutations = {
 
 const actions = {
   setSoundVolume({ commit }, volume) {
+    settings.set('videoPlayer.volume', volume)
     commit('UPDATE_VOLUME', volume)
   },
 
   setSoundMuted({ commit }, muted) {
+    settings.set('videoPlayer.muted', muted)
     commit('UPDATE_MUTED', muted)
   },
 }
