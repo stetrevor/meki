ee8a92ae391d7ad71a98005d20c870a752f60bd4
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-10-02 18:46:30 +0800

    chore: Add ffmpeg and ffprobe to asar unpack
    This is critical to make sure the app work in production. The binary
    path provided by 'ffmpeg-static' and 'ffprobe-static' won't work if
    those packages are inside of 'app.asar'.
    
    Github issue #47

--- .electron-vue/build.config.js
+++ .electron-vue/build.config.js
@@ -6,7 +6,9 @@ const path = require('path')
  */
 module.exports = {
   arch: 'x64',
-  asar: true,
+  asar: {
+    unpack: '**/node_modules/{ffmpeg-static,ffprobe-static}/**',
+  },
   dir: path.join(__dirname, '../'),
   icon: path.join(__dirname, '../build/icons/icon'),
   ignore: /(^\/(src|test|\.[a-z]+|README|yarn|static|dist\/web|coverage|temp))|\.gitkeep/,
