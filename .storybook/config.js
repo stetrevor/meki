916810c812901b05bf449ca5c9e23abe767b7bec
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-10-05 16:20:02 +0800

    chore: Stop storybook component auto registration
    Some components contain electronjs and nodejs modules that won't run in
    a browser, with Vue component auto registration, it will throw errors
    and the whole site won't work. Switching to manual registration will
    make storybook run again.
    
    Github issue #44

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,22 +3,11 @@ import { setOptions } from '@storybook/addon-options'
 import Vue from 'vue'
 import Vuex from 'vuex'
 import VueRx from 'vue-rx'
-import upperFirst from 'lodash/upperFirst'
-import camelCase from 'lodash/camelCase'
-import path from 'path'
 
 import '../src/renderer/theme.scss'
 
 Vue.use(Vuex)
 Vue.use(VueRx)
-
-const requireComponent = require.context('../src/renderer', true, /\.vue$/)
-
-requireComponent.keys().forEach(fileName => {
-  const componentConfig = requireComponent(fileName)
-  const componentName = upperFirst(camelCase(path.basename(fileName, '.vue')))
-  Vue.component(componentName, componentConfig.default || componentConfig)
-})
 
 setOptions({
   hierarchyRootSeparator: /\|/,
