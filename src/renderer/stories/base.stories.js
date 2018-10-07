dee0c58d203c2d2f501a4e0874a726bb92b7cab5
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-10-07 12:45:45 +0800

    feat: Update IconButton stories
    Github issue #47

--- src/renderer/stories/base.stories.js
+++ src/renderer/stories/base.stories.js
@@ -1,4 +1,5 @@
 import { storiesOf } from '@storybook/vue'
+import { withKnobs, boolean } from '@storybook/addon-knobs'
 import Vue from 'vue'
 
 import IconButton from '../components/Base/IconButton'
@@ -7,7 +8,11 @@ Vue.component('IconButton', IconButton)
 
 const base = storiesOf('Base Components', module)
 
-base.add('Icon', () => {
+const iconButton = storiesOf('Base Components | IconButton', module)
+
+iconButton.addDecorator(withKnobs)
+
+iconButton.add('Icon', () => {
   require('../assets/icons/icon-settings.svg')
   require('../assets/icons/icon-search.svg')
   require('../assets/icons/icon-selection-mode.svg')
@@ -17,7 +22,7 @@ base.add('Icon', () => {
   return {
     template: `
       <div class="theme-text-color-on-primary theme-bg-color-primary"
-           style="height: calc(100vh - 24px * 2); padding: 24px 0; display: grid; grid-gap: 24px; grid-template-columns: repeat(auto-fill, 48px); grid-auto-rows: 48px; justify-content: center">
+           style="width: calc(100vw - 24 * 2); height: calc(100vh - 24px * 2); padding: 24px; display: grid; grid-gap: 24px; grid-template-columns: repeat(auto-fill, 48px); grid-auto-rows: 48px; justify-content: center">
         <svg width="48" height="48"><use xlink:href="#icon-settings"/></svg>
         <svg width="48" height="48"><use xlink:href="#icon-search"/></svg>
         <svg width="48" height="48"><use xlink:href="#icon-selection-mode"/></svg>
@@ -27,28 +32,45 @@ base.add('Icon', () => {
   }
 })
 
-base.add('Icon Button', () => {
+iconButton.add('Icon Button', () => {
   require('../assets/icons/icon-settings.svg')
   require('../assets/icons/icon-search.svg')
   require('../assets/icons/icon-selection-mode.svg')
   require('../assets/icons/icon-fullscreen.svg')
   require('../assets/icons/icon-fullscreen-exit.svg')
 
+  const disabled = boolean('Disabled', false)
+
   return {
     template: `
-    <div class="theme-bg-color-primary"
-         style="height: calc(100vh - 24px * 2); padding: 24px 0; display: grid; grid-gap: 24px; grid-template-columns: repeat(auto-fill, 48px); grid-auto-rows: 48px; justify-content: center">
-      <icon-button icon="settings"/>
-      <icon-button icon="search"/>
-      <icon-button icon="selection-mode"/>
-      <icon-button icon="fullscreen"/>
-      <icon-button icon="fullscreen-exit"/>
-      <icon-button icon="fullscreen-exit" :disabled="true"/>
+    <div class=""
+         style="width: calc(100vw - 24 * 2); height: calc(100vh - 24px * 2); padding: 24px; display: grid; grid-gap: 24px; grid-template-columns: repeat(auto-fill, 96px); grid-auto-rows: 96px">
+      <div class="theme-bg-color-primary" style="width: 100%; height: 100%; display: flex; ; align-items: center; justify-content: center">
+        <icon-button :disabled="${disabled}" theme="on-primary" icon="settings"/>
+      </div>
+      <div class="theme-bg-color-primary-lighter" style="width: 100%; height: 100%; display: flex; ; align-items: center; justify-content: center; border: 1px solid">
+        <icon-button :disabled="${disabled}" theme="on-primary-lighter" icon="settings"/>
+      </div>
+      <div class="theme-bg-color-secondary" style="width: 100%; height: 100%; display: flex; ; align-items: center; justify-content: center; border: 1px solid">
+        <icon-button :disabled="${disabled}" theme="on-secondary" icon="settings"/>
+      </div>
+      <div class="theme-bg-color-background" style="width: 100%; height: 100%; display: flex; ; align-items: center; justify-content: center; border: 1px solid">
+        <icon-button :disabled="${disabled}" theme="on-background" icon="settings"/>
+      </div>
+      <div class="" style="width: 100%; height: 100%; display: flex; ; align-items: center; justify-content: center; border: 1px solid">
+        <icon-button :disabled="${disabled}" theme="primary" icon="settings"/>
+      </div>
+      <div class="" style="width: 100%; height: 100%; display: flex; ; align-items: center; justify-content: center; border: 1px solid">
+        <icon-button :disabled="${disabled}" theme="primary-lighter" icon="settings"/>
+      </div>
+      <div class="" style="width: 100%; height: 100%; display: flex; ; align-items: center; justify-content: center; border: 1px solid">
+        <icon-button :disabled="${disabled}" theme="secondary" icon="settings"/>
+      </div>
     </div>`,
   }
 })
 
-base.add('Icon Button | Toggle', () => {
+iconButton.add('Icon Button | Toggle', () => {
   require('../assets/icons/icon-settings.svg')
   require('../assets/icons/icon-search.svg')
   require('../assets/icons/icon-selection-mode.svg')
@@ -58,7 +80,7 @@ base.add('Icon Button | Toggle', () => {
   return {
     template: `
     <div class="theme-bg-color-primary"
-         style="height: calc(100vh - 24px * 2); padding: 24px 0; display: grid; grid-gap: 24px; grid-template-columns: repeat(auto-fill, 48px); grid-auto-rows: 48px; justify-content: center">
+         style="width: calc(100vw - 24 * 2); height: calc(100vh - 24px * 2); padding: 24px; display: grid; grid-gap: 24px; grid-template-columns: repeat(auto-fill, 48px); grid-auto-rows: 48px; justify-content: center">
          <icon-button :toggled="toggled" @clicked="toggled = !toggled" icon="fullscreen" icon-toggled="fullscreen-exit"/>
          <icon-button :toggled="toggled" @clicked="toggled = !toggled" icon="search" icon-toggled="settings"/>
          <icon-button :toggled="toggled" @clicked="toggled = !toggled" icon="selection-mode" icon-toggled="fullscreen-exit"/>
@@ -72,13 +94,13 @@ base.add('Icon Button | Toggle', () => {
   }
 })
 
-base.add('Overlay Icon Button', () => {
+iconButton.add('Overlay Icon Button', () => {
   require('../assets/icons/icon-play.svg')
   require('../assets/icons/icon-selection-mode.svg')
 
   return {
     template: `
-    <div style="background-image: url('https://placekitten.com/3000/2000'); height: calc(100vh - 24px * 2); padding: 24px; display: grid; grid-gap: 24px; grid-template-columns: repeat(auto-fit, 96px); justify-items: center; align-items: center; grid-auto-rows: 96px">
+    <div style="background-image: url('https://placekitten.com/3000/2000'); height: calc(100vh - 24px * 2); padding: 24px; display: grid; grid-gap: 24px; grid-template-columns: repeat(auto-fit, min(96px)); justify-items: center; align-items: center; grid-auto-rows: 96px">
       <overlay-icon-button icon="play" style="width: 48px; height: 48px"/>
       <overlay-icon-button icon="play" style="width: 96px; height: 96px"/>
       <overlay-icon-button icon="selection-mode" style="width: 48px; height: 48px"/>
