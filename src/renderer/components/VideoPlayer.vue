f4d1c07496e5d39a912087d7e6034689d32269d9
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-10-07 13:07:54 +0800

    refactor: Rename VideoPlayer class names
    Github issue #45

--- src/renderer/components/VideoPlayer.vue
+++ src/renderer/components/VideoPlayer.vue
@@ -1,32 +1,32 @@
 <template>
   <div v-stream:mousemove="{ subject: mousemove$, options: { capture: true } }" 
        v-stream:mouseup="{ subject: mouseup$, options: { capture: true } }"
-       :class="['player-page', { 'player-page--do-not-disturb': !controlsShow$ }]">
+       :class="['video-player', { 'video-player--do-not-disturb': !controlsShow$ }]">
     <video ref="video" 
            :src="videoPath"
-           class="player-page__video"
+           class="video-player__video"
            @timeupdate="progress = $refs.video.currentTime"
            @ended="paused = $refs.video.paused"/>
 
     <transition name="fade-out-in" 
                 mode="out-in">
       <div v-show="controlsShow$ || paused" 
-           class="player-page__controls">
-        <div class="player-page__header">
+           class="video-player__controls">
+        <div class="video-player__header">
           <icon-button icon="back" 
                        @clicked="exit"/>
-          <h5 class="player-page__title">{{ video.title }}</h5>
-          <icon-button class="player-page__home" 
+          <h5 class="video-player__title">{{ video.title }}</h5>
+          <icon-button class="video-player__home" 
                        icon="logo"
                        @clicked="exit"/>
         </div>
 
         <overlay-icon-button v-show="paused" 
-                             class="player-page__main-action"
+                             class="video-player__main-action"
                              icon="play"
                              @click.native="play"/>
     
-        <div class="player-page__footer">
+        <div class="video-player__footer">
           <icon-button :toggled="!paused" 
                        icon="play-arrow" 
                        icon-toggled="pause"
@@ -35,11 +35,11 @@
                        :max="video.runtime"
                        :value="progress"
                        :format="toTime"
-                       class="player-page__timeline"/>
-          <div class="player-page__progress">
+                       class="video-player__timeline"/>
+          <div class="video-player__progress">
             {{ progress | toTime }} / {{ video.runtime | toTime }}
           </div>
-          <div class="player-page__volume-controls">
+          <div class="video-player__volume-controls">
             <icon-button :toggled="videoMuted" 
                          icon="volume" 
                          icon-toggled="muted"
@@ -48,7 +48,7 @@
                          :max="100" 
                          :discrete="true"
                          :format="v => `${v}%`"
-                         class="player-page__volume"
+                         class="video-player__volume"
                          @value-changed="$refs.video.volume = $event / 100"/>
           </div>
 
@@ -57,7 +57,7 @@
                        @clicked.stop="subtitleMenuShow = !subtitleMenuShow"/>
           <subtitle-menu v-show="subtitleMenuShow" 
                          :subtitles="subtitles"
-                         class="player-page__subtitle-menu"
+                         class="video-player__subtitle-menu"
                          @active-subtitle-changed="setActiveSubtitle"
                          @dismiss="subtitleMenuShow = false"/>
 
@@ -269,7 +269,7 @@ export default {
 <style lang="scss">
 @import '../theme';
 
-.player-page {
+.video-player {
   position: relative;
   width: 100vw;
   height: 100vh;
