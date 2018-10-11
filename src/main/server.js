91e39703fac7a0f755a15f1eb88b88b03aca1bfd
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-10-11 15:55:51 +0800

    feat: Add headers for file server reponse
    The 'Access-Control-Allow-Origin' header makes <track> element loading
    errors go away.
    
    Github issue #39

--- src/main/server.js
+++ src/main/server.js
@@ -4,7 +4,11 @@ import send from 'send'
 
 export default function createServer() {
   const server = http.createServer(function onRequest(req, res) {
-    send(req, req.url).pipe(res)
+    send(req, req.url)
+      .on('headers', (res, path, stat) => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+      })
+      .pipe(res)
   })
 
   return server
