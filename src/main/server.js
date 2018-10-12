65458fcb36e5e687207d36cbf201827af0ed37a5
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-10-12 22:24:00 +0800

    feat: Transform srt request to vtt response
    The server also sets header 'Access-Control-Allow-Origin' to '*' in
    order to make client side code work.
    
    Github issue #39

--- src/main/server.js
+++ src/main/server.js
@@ -1,14 +1,26 @@
 import http from 'http'
+import fs from 'fs'
+import querystring from 'querystring'
 
 import send from 'send'
+import srt2vtt from 'srt-to-vtt'
 
 export default function createServer() {
   const server = http.createServer(function onRequest(req, res) {
-    send(req, req.url)
-      .on('headers', (res, path, stat) => {
-        res.setHeader('Access-Control-Allow-Origin', '*')
-      })
-      .pipe(res)
+    if (req.url.endsWith('.srt')) {
+      const filePath = querystring.unescape(req.url.slice(1))
+      const stream = fs.createReadStream(filePath)
+
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.setHeader('Content-Type', 'text/vtt')
+      stream.pipe(srt2vtt()).pipe(res)
+    } else {
+      send(req, req.url)
+        .on('headers', (res, path, stat) => {
+          res.setHeader('Access-Control-Allow-Origin', '*')
+        })
+        .pipe(res)
+    }
   })
 
   return server
