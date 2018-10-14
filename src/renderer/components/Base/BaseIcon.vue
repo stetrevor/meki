2a7f3c61280e9157066f3a9e6b3306df03638a96
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-10-14 18:00:43 +0800

    feat: Create BaseIcon
    Github issue #57

---
+++ src/renderer/components/Base/BaseIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg class="base-icon">
+    <use :xlink:href="`#icon-${icon}`"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'BaseIcon',
+
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.base-icon {
+  width: 24px;
+  height: 24px;
+}
+</style>
