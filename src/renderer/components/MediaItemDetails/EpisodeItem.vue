74dcb9a48842ff41474f309c81f25336ab319554
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-10-22 13:59:18 +0800

    feat: Add episode info and actions to EpisodeItem
    Github issue #67

--- src/renderer/components/MediaItemDetails/EpisodeItem.vue
+++ src/renderer/components/MediaItemDetails/EpisodeItem.vue
@@ -1,23 +1,68 @@
 <template>
-  <div class="episode-item">
+  <div class="episode-item" 
+       @mouseenter="hovered = true" 
+       @mouseleave="hovered = false">
     <div class="episode-item__icon">
       <base-icon :icon="episode.icon"/>
     </div>
+
     <div class="episode-item__title">{{ episode.title }}</div>
+
+    <div v-show="hovered" 
+         class="episode-item__info">
+      <div class="episode-item__duration">{{ episode.duration | toTime }}</div>
+      <progress-bar :progress="episode.progress" 
+                    :max="episode.duration" 
+                    :colored="true"
+                    class="episode-item__progress"/>
+      <div class="episode-item__progress-message">{{ progressMsg }}</div>
+
+      <icon-button v-show="episode.progress !== episode.duration" 
+                   icon="mark-as-complete"
+                   @clicked="$emit('episode-item-mark-as-complete')"/>
+      <icon-button :toggled="episode.favorite" 
+                   icon="favorite"
+                   icon-toggled="favorited"
+                   @clicked="$emit('episode-item-favorite')"/>
+    </div>
   </div>
 </template>
 
 .<script>
 import BaseIcon from '../Base/BaseIcon'
+import IconButton from '../Base/IconButton'
+import ProgressBar from '../Base/ProgressBar'
+
+import { toTime } from '../../filters'
 
 export default {
   name: 'EpisodeItem',
 
-  components: { BaseIcon },
+  components: { BaseIcon, IconButton, ProgressBar },
+
+  filters: { toTime },
+
   props: {
     episode: {
       type: Object,
       required: true,
+    },
+  },
+
+  data() {
+    return {
+      hovered: false,
+    }
+  },
+
+  computed: {
+    progressMsg() {
+      const { progress, duration } = this.episode
+      const secLeft = parseInt(duration - progress)
+      const minLeft = parseInt((duration - progress) / 60)
+      const msg = minLeft ? `${minLeft}m Left` : `${secLeft}s Left`
+
+      return progress === duration ? 'Watched' : msg
     },
   },
 }
@@ -29,11 +74,11 @@ export default {
 .episode-item {
   box-sizing: border-box;
   width: 100%;
-  padding: 8px 16px;
+  padding: 0 16px;
   height: 56px;
   display: grid;
-  grid-template-columns: auto 1fr;
-  grid-gap: 16px;
+  grid-template-columns: auto 1fr auto 96px auto auto auto;
+  grid-gap: 8px;
   align-items: center;
   @include theme-text-color-on-primary-lighter();
   @include theme-typography-body1();
@@ -73,6 +118,19 @@ export default {
     transition-property: color, background-color;
     transition-duration: 100ms;
     transition-timing-function: $mdc-animation-standard-curve-timing-function;
+  }
+
+  &__title {
+    padding: 0 8px;
+  }
+
+  &__info {
+    display: contents;
+  }
+
+  &__progress-message {
+    margin-right: 8px;
+    @include theme-typography-subtitle1();
   }
 }
 </style>
