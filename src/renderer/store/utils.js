b759783fa5e0a12ab45763e380a4a425fe3f4bc2
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-10-23 18:19:45 +0800

    feat: Reimplement alphanumSort
    This is an adopted version from
    http://www.davekoelle.com/files/alphanum.js.
    It sorts strings respecting number orders like '1' before '15'.
    
    Github issue #67

--- src/renderer/store/utils.js
+++ src/renderer/store/utils.js
@@ -1,10 +1,37 @@
-export function alphanumSort(v1, v2) {
-  const [t1, t2] = [v1.title, v2.title]
-  if (t1 == t2) return 0
+/* From http://www.davekoelle.com/files/alphanum.js.
+ * Adopted to ES6 syntax.
+ */
+export function alphanumSort(a, b) {
+  function chunkify(t) {
+    let tz = new Array()
+    let x = 0,
+      y = -1,
+      n = 0,
+      i,
+      j
 
-  if (typeof t1 === typeof t2) {
-    return t1 < t2 ? -1 : 1
+    while ((i = (j = t.charAt(x++)).charCodeAt(0))) {
+      let m = i == 46 || (i >= 48 && i <= 57)
+      if (m !== n) {
+        tz[++y] = ''
+        n = m
+      }
+      tz[y] += j
+    }
+    return tz
   }
 
-  return typeof a < typeof b ? -1 : 1
+  const aa = chunkify(a)
+  const bb = chunkify(b)
+
+  for (let x = 0; aa[x] && bb[x]; x++) {
+    if (aa[x] !== bb[x]) {
+      const c = Number(aa[x]),
+        d = Number(bb[x])
+      if (c == aa[x] && d == bb[x]) {
+        return c - d
+      } else return aa[x] > bb[x] ? 1 : -1
+    }
+  }
+  return aa.length - bb.length
 }
