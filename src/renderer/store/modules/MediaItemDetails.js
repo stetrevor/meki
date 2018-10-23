5adbf27d4c91cf0f4a756392a60e8b879bb400b7
Trevor Wong <stephen.trevor.wong@outlook.com>
2018-10-23 16:16:44 +0800

    feat: Add MediaItemDetails store actions, getters
    Github issue #67

--- src/renderer/store/modules/MediaItemDetails.js
+++ src/renderer/store/modules/MediaItemDetails.js
@@ -1,10 +1,32 @@
+import api from '../../api'
+
 const state = {
   currentMediaItem: null,
+  episodes: [],
+  fileList: [],
+}
+
+const getters = {
+  episodes(state) {
+    return state.episodes
+  },
+
+  fileList(state) {
+    return state.fileList
+  },
 }
 
 const mutations = {
   SWITCH_CURRENT_MEDIA_ITEM(state, mediaItem) {
     state.currentMediaItem = mediaItem
+  },
+
+  RECEIVE_EPISODES(state, episodes) {
+    state.episodes = state.episodes.concat(episodes)
+  },
+
+  RECEIVE_FILE_LIST(state, fileList) {
+    state.fileList = fileList
   },
 }
 
@@ -12,6 +34,16 @@ const actions = {
   switchCurrentMediaItem({ commit }, mediaItem) {
     commit('SWITCH_CURRENT_MEDIA_ITEM', mediaItem)
   },
+
+  async getEpisodes({ commit }, mediaItemId) {
+    const episodes = await api.getEpisodes(mediaItemId)
+    commit('RECEIVE_EPISODES', episodes)
+  },
+
+  async getFileList({ commit }, dir) {
+    const fileList = await api.getFileList(dir)
+    commit('RECEIVE_FILE_LIST', fileList)
+  },
 }
 
-export default { state, mutations, actions }
+export default { state, getters, mutations, actions }
